4ec12b4263dce304481e3daa9d2d339355da7b72
Syed Muhammad Arslan Haider <arslan.sheerazi92@gmail.com>
2020-11-06 11:01:27 +0100

    maps added for shiny

--- inst/shiny-examples/app.R
+++ inst/shiny-examples/app.R
@@ -7,43 +7,65 @@
 setwd('../..')
 #print(getwd())
 source("R/fetch_data.R",encoding = getOption("encoding"))
-data <- fetch_api_data()
+df_crime <- fetch_api_data()
 
-
+# loading libaries
 library(shiny)
-# Define UI for application that draws a histogram
+library(leaflet.minicharts)
+library(leaflet)
+library(stringr)
+
+#r_colors <- rgb(t(col2rgb(colors()) / 255))
+#names(r_colors) <- colors()
+
+# Ui for shiny application
+# ui <- navbarPage("Crime Analytics",
+#                  tabPanel("Map"),
+#                  tabPanel("Plots"),
+#                  header = "Crime Data visulizatuin of Maryland Data",
+#                  footer = "Made by Arslan and Zahra."
+#                  #tabPanel("Component 3")
+# )
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Uniform Crime Reporting"),
-
-    # Sidebar with a slider input for number of bins 
+    titlePanel("Crime Analytics", "Interactive Shiny Application"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("var",
-                        "select crime type:",
-                        list("crime_type1","crime_type2", "crime_type3"))
-                        
-        ,),
-
-        # Show a plot of the generated distribution
+            selectInput("selectcity",
+                        h4("City"),
+                        choices = list(),
+                        )
+        ),
         mainPanel(
-           textOutput("selected_var"),
-           plotOutput("barplot1")
+            leafletOutput("map")
         )
     )
 )
-
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-    colums <- colnames(data)
-    
-    output$barplot1 <- renderPlot(
-        barplot(table(data[4]))
-    )
-    
-    output$selected_var <- renderText({
-       paste( "You have selected ", input$var)
+server <- function(input, output, session) {
+    #colums <- colnames(data)
+    # discard the observations with NA values 
+    df_comp_crime <- df_crime[complete.cases(df_crime), ]
+    #order_df <- df_comp_crime[order(City),]
+    cities <- unique(as.vector(df_comp_crime["City"]))
+    cities <- as.list(cities)
+    cities <- append("All Cities",cities[1]) 
+    #populate the list of cities
+    updateSelectInput(session,
+                      "selectcity",
+                      label = paste("Select City"),
+                      choices = cities)
+    # add pins to map
+    output$map <- renderLeaflet({
+        
+        points <- eventReactive(input$recalc, {
+            cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
+        }, ignoreNULL = FALSE)
+        
+        leaflet() %>%
+            addProviderTiles(providers$Stamen.TonerLite,
+                             options = providerTileOptions(noWrap = TRUE)
+            ) %>%
+            addMarkers(data = points())
     })
 }
 
